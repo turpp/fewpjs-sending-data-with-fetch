f646eb4964d74172273909093f6a8f78facd93ef
turpp <white.tripp@icloud.com>
2021-01-12 15:24:50 -0600

    commit tree dd3fe3eccf8c4fd57548c379a6f3d1046253c50f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users'), {
+        method: 'POST',
+        // headers: {
+        //     "Content-Type": "application/json",
+        //     "Accept": "application/json"
+        // }, 
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail 
+        })
+    }
+}
